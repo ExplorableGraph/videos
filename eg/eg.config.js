e9c83d04300baae552f2bb17ff6f24571f4a760d
Jan Miksovsky <jan@component.kitchen>
2021-07-04 16:30:33 -0700

    Track changes

--- eg/eg.config.js
+++ eg/eg.config.js
@@ -1,39 +1,6 @@
-import {
-  builtins,
-  Compose,
-  ExplorableFiles,
-  ModulesDefaultExportMixin,
-} from "@explorablegraph/explorable";
-import path from "path";
+import { builtins, CommandModules, Compose } from "@explorablegraph/explorable";
 import process from "process";
 
-function CommandsMixin(Base) {
-  return class Commands extends Base {
-    async *[Symbol.asyncIterator]() {
-      for await (const key of super[Symbol.asyncIterator]()) {
-        if (key.endsWith(".js")) {
-          yield path.basename(key, ".js");
-        }
-      }
-    }
-
-    async get(...keys) {
-      // Try keys as they are.
-      // const value = await super.get(...keys);
-      // if (value !== undefined) {
-      //   return value;
-      // }
-      const lastKey = keys.pop();
-      const commandKey = `${lastKey}.js`;
-      keys.push(commandKey);
-      const value = await super.get(...keys);
-      return value;
-    }
-  };
-}
-
-const commands = new (CommandsMixin(
-  ModulesDefaultExportMixin(ExplorableFiles)
-))(process.cwd());
-
-export default new Compose(commands, builtins);
+// Add all commands in the current folder to the active eg scope. E.g., if the
+// current folder contains "foo.js", then eg will gain a `foo` command.
+export default new Compose(new CommandModules(process.cwd()), builtins);
